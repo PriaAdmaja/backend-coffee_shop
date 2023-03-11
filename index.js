f230a79a128566621305903ff5409df53d98079c
Pria Admaja <109842306+PriaAdmaja@users.noreply.github.com>
2023-03-11 11:41:57 +0700

    add cors

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 require('dotenv').config();
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
 const app = express();
 const port = 8080;
 
+
+app.use(cors())
+app.use(bodyParser.json())
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
