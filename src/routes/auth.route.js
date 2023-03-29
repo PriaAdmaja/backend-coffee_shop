f0c624638da73fbe35eeaf789d9c1197e755c4f3
Pria Admaja <109842306+PriaAdmaja@users.noreply.github.com>
2023-03-29 16:22:32 +0700

    update auth

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -5,7 +5,7 @@ const authController = require('../controllers/auth.controller')
 const authMiddleware = require('../middlewares/auth')
 
 authRouter.get("/private", authMiddleware.checkToken, authController.privateAccess);
-authRouter.get("/forgotpassword", authController.forgotPassword);
+authRouter.patch("/forgotpassword", authController.forgotPassword);
 authRouter.post("/login", authController.login);
 authRouter.post("/register", authController.register);
 authRouter.post("/logout", authMiddleware.checkToken, authController.logout)
