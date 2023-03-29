789dfa77683906d6c926fa36376fea71bc1fa394
Pria Admaja <109842306+PriaAdmaja@users.noreply.github.com>
2023-03-29 08:08:45 +0700

    edit

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-const express = require('express');
 require('dotenv').config();
-const bodyParser = require('body-parser')
+const express = require('express');
 const cors = require('cors')
+const bodyParser = require('body-parser')
+const morgan = require("morgan");
 
-const app = express();
 const port = 8080;
-
+const app = express();
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-const morgan = require("morgan");
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms"));
 
 const masterRouter = require('./src/routes/index');
