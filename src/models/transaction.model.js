a820ba4c9641cd38660b2264a508f4472b74aceb
Pria Admaja <109842306+PriaAdmaja@users.noreply.github.com>
2023-04-03 17:14:04 +0700

    fix transaction

--- src/models/transaction.model.js
+++ src/models/transaction.model.js
@@ -1,9 +1,9 @@
 
 const createTransaction = (client, body, userId) => {
     return new Promise((resolve, reject) => {
-        const sql = `insert into "transaction" (users_id, promos_id, payment_id, deliveries_id, notes, created_at)
-        values ($1, $2, $3, $4, $5, now()) returning id`;
-        const values = [userId, body.promoId, body.paymentId, body.deliveryId, body.address, body.notes]
+        const sql = `insert into "transaction" (users_id, promos_id, payment_id, deliveries_id,  notes, grand_total, created_at)
+        values ($1, $2, $3, $4, $5, $6, now()) returning id`;
+        const values = [userId, body.promoId, body.paymentId, body.deliveryId, body.notes, body.grandTotal]
         client.query(sql, values, (err, result) => {
             if (err) {
                 return reject(err);
@@ -16,13 +16,13 @@ const createTransaction = (client, body, userId) => {
 const createCart = (client, body, transactionId) => {
     return new Promise((resolve, reject) => {
         const { products } = body
-        let sql = `insert into cart (transaction_id, product_id, size_id, quantity, subtotal) values `;
+        let sql = `insert into cart (transaction_id, product_id, size_id, quantity, price) values `;
         let values = [];
         products.forEach((product, i) => {
-            const { productId, sizeId, quantity, subtotal } = product;
+            const { productId, sizeId, quantity, price } = product;
             if (values.length) sql += ", ";
             sql += `($${1 + 5 * i}, $${2 + 5 * i}, $${3 + 5 * i}, $${4 + 5 * i}, $${5 + 5 * i})`;
-            values.push(transactionId, productId, sizeId, quantity, subtotal);
+            values.push(transactionId, productId, sizeId, quantity, price);
         });
        
         client.query(sql, values, (err) => {
@@ -36,7 +36,7 @@ const createCart = (client, body, transactionId) => {
 
 const getTransactionDetail = (client, transactionId) => {
     return new Promise((resolve, reject) => {
-        const sql = `select u.email, b.address, p3."name" as "product", s2."size", p2."method" as "payment", c.quantity, p.coupon_code as "promo", c.subtotal from cart c 
+        const sql = `select u.email, b.address, p3."name" as "product", s2."size", p2."method" as "payment", c.quantity, p.coupon_code as "promo", c.price from cart c 
         left join "transaction" t on t.id = c.transaction_id
         left join users u on u.id = t.users_id 
         left join promos p on p.id  = t.promos_id 
