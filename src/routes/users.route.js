91afa7abf289f3642a18ba684172913f2db442b1
Pria Admaja <109842306+PriaAdmaja@users.noreply.github.com>
2023-06-04 18:47:15 +0700

    add error handling on update avatar

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -5,10 +5,11 @@ const usersController = require('../controllers/users.controller');
 const authMiddleware = require('../middlewares/auth');
 const memoryUpload = require('../middlewares/memoryUpload');
 const cloudMiddleware = require('../middlewares/cloud')
+const fileSizeLimitErrorHandler = require('../middlewares/fileSizeErrorHandler')
 
 usersRoute.get('/', usersController.getUsers);
 usersRoute.get('/:id', usersController.findUsers);
-usersRoute.patch('/:id', authMiddleware.checkToken, memoryUpload.single("image"), cloudMiddleware.cloudUploadUser, usersController.updateUsers);
+usersRoute.patch('/:id', authMiddleware.checkToken, memoryUpload.single("image"), fileSizeLimitErrorHandler.fileSizeLimitErrorHandler, cloudMiddleware.cloudUploadUser, usersController.updateUsers);
 usersRoute.delete('/:id', usersController.deleteUsers);
 
 module.exports = usersRoute;
